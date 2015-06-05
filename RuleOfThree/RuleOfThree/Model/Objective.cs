321ada91a8856b9b0b7f90d51f552e37e2f84d05
Kym Phillpotts <kphillpotts@gmail.com>
2015-06-05 16:11:14 +1000

    Added Objective Object

---
+++ RuleOfThree/RuleOfThree/Model/Objective.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RuleOfThree
+{
+    public enum ObjectiveStatus
+    {
+        None = 0,
+        InProgress = 1,
+        PushedForward = 2,
+        Done = 3
+    }
+
+    public class Objective
+    {
+        public Objective()
+        {
+        }
+
+        public string Title
+        {
+            get;
+            set;
+        }
+        public ObjectiveStatus Status
+        {
+            get;
+            set;
+        }
+    }
+}
+
