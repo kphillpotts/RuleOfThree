004361ad1c5589fe27ff4675f1f072263148c7d0
Kym Phillpotts <kphillpotts@gmail.com>
2015-06-05 16:03:48 +1000

    Initial project creation

---
+++ RuleOfThree/RuleOfThree.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using RuleOfThree.WinPhone.Resources;
+
+namespace RuleOfThree.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
